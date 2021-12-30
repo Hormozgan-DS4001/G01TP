d020425839ff958253dfbd1a512944570810e720
kamandkrg <kamandkargar@gmail.com>
2021-12-30 15:38:50 +0330

    write attribute class Camera

--- model.py
+++ model.py
@@ -1,8 +1,15 @@
 
 
 class Camera:
-    def __init__(self, name, address, out, ):
-        pass
+    def __init__(self, name: str, address: str, code: int, out: bool, max_speed_truck: int = None,
+                 max_speed_car: int = None, min_speed: int = None):
+        self.name = name
+        self.address = address
+        self.code = code
+        self.out = out
+        self.max_speed_truck = max_speed_truck
+        self.max_speed_car = max_speed_car
+        self.min_speed = min_speed
 
 
 class Car:
