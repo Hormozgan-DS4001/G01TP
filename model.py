66cc21b28c3400a3eedb13a941cebc798bba432a
kamandkrg <kamandkargar@gmail.com>
2021-12-30 13:28:45 +0330

    start project

---
+++ model.py
@@ -0,0 +1,17 @@
+
+
+class Camera:
+    def __init__(self, name, address, out, ):
+        pass
+
+
+class Car:
+    def __init__(self):
+        pass
+
+
+class Core:
+    def __init__(self):
+        pass
+
+
