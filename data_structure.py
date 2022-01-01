2cabc8b37524f2950485056e7fe19a7a1ca25b9e
kamandkrg <kamandkargar@gmail.com>
2022-01-01 17:00:46 +0330

    add hashtable to data structure

--- data_structure.py
+++ data_structure.py
@@ -43,8 +43,87 @@ class HashTable:
             self.key = key
             self.value = value
 
-    def __init__(self, size: int = 2000):
+    def __init__(self, size: int = 20):
         self.array: List[Optional[HashTable._Node]] = [None] * size
+        self.size = size
+        self.count = 0
+
+    def __getitem__(self, key):
+        for i in range(self.size):
+            index = self._hash_function(key, i)
+            if self.array[index] is None:
+                return
+            elif self.array[index] == "deleted":
+                continue
+            elif self.array[index].key == key:
+                return self.array[index].value
+
+    def __setitem__(self, key, value):
+        if self.count >= 0.75 * self.size:
+            self._extend()
+        new_node = HashTable._Node(key, value)
+        for i in range(self.size):
+            index = self._hash_function(key, i)
+            if self.array[index] is None or self.array[index] == "deleted":
+                self.array[index] = new_node
+                self.count += 1
+                return
+            elif self.array[index].key == key:
+                self.array[index].value = value
+                return
+        raise ValueError("Hashtable is full")
+
+    def __iter__(self):
+        count = -1
+        while count < self.size:
+            count += 1
+            if self.array[count] is None:
+                continue
+            else:
+                yield self.array[count].key, self.array[count].value
+
+    def __contains__(self, item):
+        if item in self.key():
+            return True
+        return False
+
+    def _hash_function(self, x, i):
+        return (hash(x) + i) % self.size
+
+    def _extend(self):
+        old_array = self.array
+        self.size *= 2
+        self.array = [None] * self.size
+        self.count = 0
+        for element in old_array:
+            if isinstance(element, HashTable._Node):
+                for i in range(self.size):
+                    index = self._hash_function(element.key, i)
+                    if self.array[index] is None:
+                        self.array[index] = element
+                        self.count += 1
+                        break
+
+    def key(self):
+        count = -1
+        while count < self.size:
+            count += 1
+            if self.array[count] is None:
+                continue
+            else:
+                yield self.array[count].key
+
+
+class HashTrie:
+    class _Node:
+        def __init__(self, key, value):
+            self.key = key
+            self.value = value
+            self.children = {}
+            self.value = Sll()
+
+    def __init__(self, size: int = 2000):
+        self.array: List[Optional[HashTrie._Node]] = [None] * size
         self.size = size
         self.count = 0
 
@@ -72,7 +151,7 @@ class HashTable:
 
         if self.count >= 0.75 * self.size:
             self._extend()
-        new_node = HashTable._Node(key, value)
+        new_node = HashTrie._Node(key, value)
         for i in range(self.size):
             index = self._hash_function(key, i)
             if self.array[index] is None:
@@ -103,7 +182,7 @@ class HashTable:
         self.array = [None] * self.size
         self.count = 0
         for element in old_array:
-            if isinstance(element, HashTable._Node):
+            if isinstance(element, HashTrie._Node):
                 for i in range(self.size):
                     index = self._hash_function(element.key, i)
                     if self.array[index] is None:
@@ -128,27 +207,4 @@ class HashTable:
                 continue
             else:
                 yield self.array[count].key
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
