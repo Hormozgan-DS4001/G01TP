027047f8b7ce7363053ed8557a0338db2e2a684e
kamandkrg <kamandkargar@gmail.com>
2022-01-05 13:24:54 +0330

    add method to class Car

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
+from data_structure import Sll, HashTable, HashTableCamera, Trie
 
 
 class Camera:
-    def __init__(self, name: str, address: str, code: int, out: bool, max_speed_truck: int = None,
+    def __init__(self, name: str, address: str, code: int, out, max_speed_truck: int = None,
                  max_speed_car: int = None, min_speed: int = None):
         self.name = name
         self.address = address
@@ -11,6 +12,18 @@ class Camera:
         self.max_speed_car = max_speed_car
         self.min_speed = min_speed
 
+    def check_speed(self, car: "Car", speed):
+        if car.check_steal():
+            return 4
+        if car.heavy() and self.max_speed_truck:
+            if speed > self.max_speed_truck:
+                car.add_violation(1)
+                return 1
+        if not car.heavy() and self.max_speed_car:
+            if speed > self.max_speed_car:
+                car.add_violation(1)
+                return 1
+
 
 class Model:
     def __init__(self, name):
@@ -18,12 +31,27 @@ class Model:
 
 
 class Car:
-    def __init__(self, model: Model, name, national_code: int, heavy: bool, tag: int):
+    def __init__(self, model: Model, name, national_code: int, heavy, tag: int, steal=False):
         self.model = model
         self.name = name
         self.national_code = national_code
         self.heavy = heavy
         self.tag = tag
+        self.steal = steal
+        self.violations = Sll()
+
+    def check_steal(self):
+        return self.steal
+
+    def steal_car(self, steal):
+        self.steal = steal
+
+    def heavy(self):
+        if self.heavy:
+            return True
+
+    def add_violation(self, violation):
+        self.violations.append(violation)
 
 
 class Core:
