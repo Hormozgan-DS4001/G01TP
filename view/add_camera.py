0d0848db9a2b5e40715c553bac4f0e0f5d005426
kamandkrg <kamandkargar@gmail.com>
2022-01-20 13:06:39 +0330

    write init

--- view/add_camera.py
+++ view/add_camera.py
@@ -19,8 +19,19 @@ class AddCamera(Tk):
         frm_in.grid(row=2, column=0, columnspan=2)
         self.out_in = BooleanVar()
         self.out_in.set(False)
-        Radiobutton(frm_in, text="OUT", variable=self.out_in, value=True, command=self.check_out).grid(row=0, column=0)
         Radiobutton(frm_in, text="IN", variable=self.out_in, value=False, command=self.check_out).grid(row=0, column=1)
+        Radiobutton(frm_in, text="OUT", variable=self.out_in, value=True, command=self.check_out).grid(row=0, column=0)
+        frm_time = Frame(frm_in)
+        frm_time.grid(row=1, column=0, columnspan=2)
+        Label(frm_time, text="No Stop Time").grid(row=0, column=0, columnspan=4)
+        Label(frm_time, text="Hour: ").grid(row=1, column=0)
+        self.ent_h = Spinbox(frm_time, from_=00, to=23, format="%02.0f", state="readonly")
+        self.ent_h.grid(row=1, column=1)
+        Label(frm_time, text="Min: ").grid(row=1, column=2)
+        self.ent_min = Spinbox(frm_time, from_=0, to=60, state="readonly")
+        self.ent_min.grid(row=1, column=3)
+
+
 
 
         # Label(self, text="Address: ").grid(row=2, column=0)
@@ -28,7 +39,7 @@ class AddCamera(Tk):
         # self.ent_address.grid(row=2, column=1, columnspan=4)
 
     def check_out(self):
-        pass
+        if
 
 
 
